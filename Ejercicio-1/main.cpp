17c898d89fe1c5f5a9b205e15b1e00500232cc72
octavioromagnoli <octaromag@gmail.com>
2025-06-16 17:49:50 -0300

    Final format changes to main

--- Ejercicio-1/main.cpp
+++ Ejercicio-1/main.cpp
@@ -24,13 +24,16 @@ int main()
   {{"Ascuas", 4}, {"Lanzallamas", 6}, {"Giro Fuego", 5}},
   {0, 250, 1300});
   
-  cout << string(100, '=') << endl << "Se creará una Pokedex vinculada al archivo datapokedex.bin" << endl << "y se agregarán los Pokemones Squirtle, Bulbasaur y Charmander." << endl << string(100, '=') << endl;
+  cout << string(100, '=') << endl << "Se creará una Pokedex vinculada al archivo datapokedex.bin" << endl << "y se agregarán los Pokemones Squirtle, Bulbasaur y Charmander." << endl << "Notar que si ya se corrió el programa, los pokemones fueron recuperados del archivo y ya se encuentran registrados "<< endl << "(a menos que se haya borrado el archivo datapokedex.bin)." << endl << string(100, '=') << endl;
 
   Pokedex pokedex("datapokedex.bin");
   
   pokedex.agregar(Squirtle, SquirtleInfo);
   pokedex.agregar(Bulbasaur, BulbasaurInfo);
   pokedex.agregar(Charmander, CharmanderInfo);
+
+  cout << endl << string(100, '=') << endl << "Se buscarán a los pokemones Squirtle (debe estar y mostrarse su info) y"<<endl<<"Pikachu, (debe imprimir Pokemon desconocido)." << endl << string(100, '=') << endl << endl;
+
   
   Pokemon squirtle("Squirtle", 870);
   Pokemon pikachu("Pikachu", 390);
@@ -38,19 +41,15 @@ int main()
   pokedex.mostrar(pikachu);
   cout<<endl;
 
+  cout << endl << string(100, '=') << endl << "Se mostrarán todos los pokemones utilizando el método mostrarTodos()" << endl << string(100, '=') << endl << endl;
+
   pokedex.mostrarTodos();
   
-  Pokemon HIJODELVIENTO = Pokemon("HIJODELVIENTO", 633);
-  PokemonInfo HIJODELVIENTOInfo = PokemonInfo("Fuego", "Una lagartija con una llama en su cola",
-  {{"Ascuas", 4}, {"Lanzallamas", 6}, {"Giro Fuego", 5}},
-  {0, 250, 1300});
-  pokedex.agregar(HIJODELVIENTO, HIJODELVIENTOInfo);
+  cout << endl << string(100, '=') << endl << "Se creará una nueva Pokedex cargada desde el mismo archivo que la anterior." << endl << "Debe mostrar los mismos pokemones al llamar a mostrarTodos()." << endl << string(100, '=') << endl << endl;
 
-  Pokedex pimpumpam("datapokedex.bin");
+  Pokedex nuevapokedex("datapokedex.bin");
 
-  cout<< endl<< string(100, 'W') << endl;
-
-  pimpumpam.mostrarTodos();
+  nuevapokedex.mostrarTodos();
 
   return 0;
 }
